7e4d1070cd6ee2d3b426cb2773837670577d03b1
Harsh.13799 <harshspatel13799@gmail.com>
2023-08-16 00:19:12 +0530

    update files

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
 // ---
-const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
-const smallMenu = document.querySelector('.header__sm-menu')
-const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
+const smallMenu = document.querySelector(".header__sm-menu");
+const headerHamMenuBtn = document.querySelector(".header__main-ham-menu");
 const headerHamMenuCloseBtn = document.querySelector(
-  '.header__main-ham-menu-close'
-)
-const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+  ".header__main-ham-menu-close"
+);
+const headerSmallMenuLinks = document.querySelectorAll(".header__sm-menu-link");
 
-hamMenuBtn.addEventListener('click', () => {
-  if (smallMenu.classList.contains('header__sm-menu--active')) {
-    smallMenu.classList.remove('header__sm-menu--active')
+hamMenuBtn.addEventListener("click", () => {
+  if (smallMenu.classList.contains("header__sm-menu--active")) {
+    smallMenu.classList.remove("header__sm-menu--active");
   } else {
-    smallMenu.classList.add('header__sm-menu--active')
+    smallMenu.classList.add("header__sm-menu--active");
   }
-  if (headerHamMenuBtn.classList.contains('d-none')) {
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
+  if (headerHamMenuBtn.classList.contains("d-none")) {
+    headerHamMenuBtn.classList.remove("d-none");
+    headerHamMenuCloseBtn.classList.add("d-none");
   } else {
-    headerHamMenuBtn.classList.add('d-none')
-    headerHamMenuCloseBtn.classList.remove('d-none')
+    headerHamMenuBtn.classList.add("d-none");
+    headerHamMenuCloseBtn.classList.remove("d-none");
   }
-})
+});
 
 for (let i = 0; i < headerSmallMenuLinks.length; i++) {
-  headerSmallMenuLinks[i].addEventListener('click', () => {
-    smallMenu.classList.remove('header__sm-menu--active')
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
-  })
+  headerSmallMenuLinks[i].addEventListener("click", () => {
+    smallMenu.classList.remove("header__sm-menu--active");
+    headerHamMenuBtn.classList.remove("d-none");
+    headerHamMenuCloseBtn.classList.add("d-none");
+  });
 }
 
-// ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
+const headerLogoConatiner = document.querySelector(".header__logo-container");
 
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
-})
+headerLogoConatiner.addEventListener("click", () => {
+  location.href = "index.html";
+});
+
+function currentSlide(n, no) {
+  showSlides((slideIndex[no] = n), no);
+}
+
+let slideIndex = [1, 1, 1, 1, 1, 1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = [
+  "mySlides1",
+  "mySlides2",
+  "mySlides3",
+  "mySlides4",
+  "mySlides5",
+  "mySlides6",
+];
+let dotId = ["dot1", "dot2", "dot3", "dot4", "dot5", "dot6"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex[no] - 1].className += " active";
+  x[slideIndex[no] - 1].style.display = "block";
+}
